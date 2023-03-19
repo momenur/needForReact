7030328138ee1c919e605266f5ab772b61648b58
momenur <momenurislam6@gmail.com>
2023-03-19 15:01:05 +0600

    Array Concept

---
+++ js/arrayMethods.js
@@ -0,0 +1,17 @@
+// Map Concept 
+const accessory = [
+    {name: 'Laptop', band: 'Asus', price: 62000, color: 'Silver'},
+    {name: 'Mobile', band: 'Redmi', price: 14500, color: 'Black'},
+    {name: 'Watch', band: 'Titan', price: 2000, color: 'Yellow'},
+    {name: 'Power Bank', band: 'MI', price: 2200, color: 'Gry'},
+    {name: 'Sunglass', band: 'Redux', price: 6000, color: 'Pink'}
+];
+const bands = accessory.map(item => item.band);
+console.log(bands);
+
+// forEach Concept 
+accessory.forEach(product => {
+    const price = product.price;
+    const newPrice = price + 50000 ;
+    console.log(newPrice);
+});
